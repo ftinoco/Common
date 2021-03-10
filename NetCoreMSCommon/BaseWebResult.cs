38f1e211bf07e56e871c5e56809a3522d264518a
Fernando Tinoco <fernandotinoco_45@hotmail.com>
2021-03-09 23:30:14 -0600

    Initial commit

---
+++ NetCoreMSCommon/BaseWebResult.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+using System.Text.Json.Serialization;
+
+namespace NetCoreMSCommon
+{
+    public class BaseWebResult
+    {
+        public BaseWebResult()
+        {
+            Message = " ";
+            ResponseCode = HttpStatusCode.OK;
+        }
+
+        [JsonPropertyName("message")]
+        /// <summary>
+        /// Mensaje de respuesta de la petición
+        /// </summary>
+        public string Message { get; set; }
+
+        [JsonPropertyName("responseCode")]
+        /// <summary>
+        /// Código de respuesta de la petición
+        /// </summary>
+        public HttpStatusCode ResponseCode { set; get; }
+    }
+}
---
+++ NetCoreMSCommon/WebResult.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json.Serialization;
+
+namespace NetCoreMSCommon
+{
+    public class WebResult<TData> : BaseWebResult
+    {
+        public WebResult() : base() { }
+
+        public WebResult(TData data) : base()
+        {
+            Data = data;
+        }
+
+        [JsonPropertyName("data")]
+        /// <summary>
+        /// Datos de la petición
+        /// </summary>
+        public TData Data { get; set; }
+    }
+}
