aab0592a0162c6f2eabe3b5ee1b5cae9a21b9315
Fernando Tinoco <fernandotinoco_45@hotmail.com>
2021-05-22 23:36:57 -0600

    Adding service collection extensions

---
+++ NetCoreCommon/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,78 @@
+﻿using AutoMapper;
+using AutoMapper.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NetCoreCommon.AutoMapper;
+using NetCoreCommon.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace NetCoreCommon.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection DiscoverAndRegisterMappingProfiles<TProfileID>(
+            this IServiceCollection services, Assembly assembly, TProfileID defaultProfile, bool usingUniqueProfileAsDefault = true)
+        {
+            if (assembly == null)
+                throw new ArgumentNullException(nameof(assembly), "You must specify a valid assembly to perform mapping profile search and registration");
+
+            Dictionary<TProfileID, MapperConfigurationExpression> dictionary1 = new();
+            foreach (Type type in assembly.DefinedTypes.Where(x => x.IsClass && x.IsSubclassOf(typeof(AutoMapperProfile<TProfileID>))).ToList())
+            {
+                AutoMapperProfile<TProfileID> instance = Activator.CreateInstance(type) as AutoMapperProfile<TProfileID>;
+                if (dictionary1.ContainsKey(instance.ProfileID))
+                    instance.ConfigureProfile(dictionary1[instance.ProfileID]);
+                else
+                {
+                    MapperConfigurationExpression configurationExpression = new MapperConfigurationExpression();
+                    instance.ConfigureProfile(configurationExpression);
+                    dictionary1.Add(instance.ProfileID, configurationExpression);
+                }
+            }
+            Dictionary<TProfileID, IMapper> dictionary2 = new();
+
+            foreach (KeyValuePair<TProfileID, MapperConfigurationExpression> keyValuePair in dictionary1)
+                dictionary2.Add(keyValuePair.Key, new MapperConfiguration(keyValuePair.Value).CreateMapper());
+
+            DefaultAutoMapperManager<TProfileID> autoMapperManager = new(dictionary2, defaultProfile, usingUniqueProfileAsDefault);
+            services.AddSingleton((IAutoMapperManager<TProfileID>)autoMapperManager);
+            return services;
+        }
+
+        public static IServiceCollection DiscoverAndRegisterRepositories(this IServiceCollection services,
+            Assembly assembly, Type repositoryInterface, ServiceLifetime lifetime = ServiceLifetime.Transient)
+        {
+            if (assembly == null)
+                throw new ArgumentNullException(nameof(assembly), "You must specify an assembly to examine");
+
+            if (!repositoryInterface.IsGenericType || !repositoryInterface.IsInterface)
+                throw new ArgumentException($"The data type {repositoryInterface.Name} must be a generic interface");
+
+            foreach (TypeInfo typeInfo in assembly.DefinedTypes.Where(x => !x.IsInterface && !x.IsAbstract && !x.IsNested).ToList())
+            {
+                List<Type> list = typeInfo.ImplementedInterfaces.Where(x => !x.IsGenericType).ToList();
+                int num = list.SelectMany(x => x.GetInterfaces()).Where(new Func<Type, bool>(TypeValidator)).Count();
+
+                if (num > 1)
+                    throw new AmbiguousMatchException($"The data type \"{ typeInfo.Name }\" implements more than one interface of the type \"{ repositoryInterface.Name}\". The discovery and registration process automatically adds only the types that implement an interface at a time of type \"{ repositoryInterface.Name}\". Please specify manually in the service configuration the interface and its implementation to take into account for the injection of dependencies.");
+
+                if (num == 1)
+                {
+                    foreach (Type serviceType in list)
+                    {
+                        if (((IEnumerable<Type>)serviceType.GetInterfaces()).Where(new Func<Type, bool>(TypeValidator)).Any())
+                        {
+                            services.Add(new ServiceDescriptor(serviceType, typeInfo, lifetime));
+                            break;
+                        }
+                    }
+                }
+            }
+            return services;
+
+            bool TypeValidator(Type type) => type.IsGenericType ? type.GetGenericTypeDefinition().FullName == repositoryInterface.FullName : type.FullName == repositoryInterface.FullName;
+        }
+    }
+}
---
+++ NetCoreCommon/Helpers/DefaultAutoMapperManager.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using NetCoreCommon.AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NetCoreCommon.Helpers
+{
+    public class DefaultAutoMapperManager<TProfileID> : IAutoMapperManager<TProfileID>
+    {
+        private readonly bool _defaultIsSet;
+        private readonly IMapper _defaultMapper;
+
+        public DefaultAutoMapperManager(IDictionary<TProfileID, IMapper> profiles, TProfileID defaultProfile,
+                                        bool usingUniqueProfileAsDefault = true)
+        {
+            _defaultIsSet = false;
+            Profile = profiles != null ? profiles : new Dictionary<TProfileID, IMapper>();
+            if (profiles.Count < 1)
+                return;
+            if (profiles.Count == 1 & usingUniqueProfileAsDefault)
+            {
+                _defaultMapper = Profile.First().Value;
+                _defaultIsSet = true;
+            }
+            else
+            {
+                _defaultMapper = Profile.ContainsKey(defaultProfile) ? Profile[defaultProfile] : 
+                                    throw new IndexOutOfRangeException("The AutoMapper profile list does not have a profile with the identifier specified by default");
+                _defaultIsSet = true;
+            }
+        }
+
+        public IMapper Default
+        {
+            get
+            {
+                if (_defaultIsSet)
+                    return _defaultMapper;
+
+                throw new InvalidOperationException("A default profile has not been established. Please review the service configuration and verify if the list of profiles has at least one");
+            }
+        }
+
+        public IDictionary<TProfileID, IMapper> Profile { get; }
+    }
+}
