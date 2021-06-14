93354072acbf82a67181e61bdd50a81516aa95d8
Fernando Tinoco <fernandotinoco_45@hotmail.com>
2021-06-13 21:04:19 -0600

    Adding predicate builder

---
+++ NetCoreCommon/Helpers/PredicateBuilder.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace NetCoreCommon.Helpers
+{
+    /// <summary>  
+    /// Enables the efficient, dynamic composition of query predicates.  
+    /// </summary>  
+    public static class PredicateBuilder
+    {
+        /// <summary>  
+        /// Creates a predicate that evaluates to true.  
+        /// </summary>  
+        public static Expression<Func<T, bool>> True<T>() { return param => true; }
+
+        /// <summary>  
+        /// Creates a predicate that evaluates to false.  
+        /// </summary>  
+        public static Expression<Func<T, bool>> False<T>() { return param => false; }
+
+        /// <summary>  
+        /// Creates a predicate expression from the specified lambda expression.  
+        /// </summary>  
+        public static Expression<Func<T, bool>> Create<T>(Expression<Func<T, bool>> predicate) { return predicate; }
+
+        /// <summary>  
+        /// Combines the first predicate with the second using the logical "and".  
+        /// </summary>  
+        public static Expression<Func<T, bool>> And<T>(this Expression<Func<T, bool>> first, Expression<Func<T, bool>> second)
+        {
+            return first.Compose(second, Expression.AndAlso);
+        }
+
+        /// <summary>  
+        /// Combines the first predicate with the second using the logical "or".  
+        /// </summary>  
+        public static Expression<Func<T, bool>> Or<T>(this Expression<Func<T, bool>> first, Expression<Func<T, bool>> second)
+        {
+            return first.Compose(second, Expression.OrElse);
+        }
+
+        /// <summary>  
+        /// Negates the predicate.  
+        /// </summary>  
+        public static Expression<Func<T, bool>> Not<T>(this Expression<Func<T, bool>> expression)
+        {
+            var negated = Expression.Not(expression.Body);
+            return Expression.Lambda<Func<T, bool>>(negated, expression.Parameters);
+        }
+
+        /// <summary>  
+        /// Combines the first expression with the second using the specified merge function.  
+        /// </summary>  
+        static Expression<T> Compose<T>(this Expression<T> first, Expression<T> second, Func<Expression, Expression, Expression> merge)
+        {
+            // zip parameters (map from parameters of second to parameters of first)  
+            var map = first.Parameters
+                .Select((f, i) => new { f, s = second.Parameters[i] })
+                .ToDictionary(p => p.s, p => p.f);
+
+            // replace parameters in the second lambda expression with the parameters in the first  
+            var secondBody = ParameterRebinder.ReplaceParameters(map, second.Body);
+
+            // create a merged lambda expression with parameters from the first expression  
+            return Expression.Lambda<T>(merge(first.Body, secondBody), first.Parameters);
+        }
+
+        private static IOrderedQueryable<T> OrderingHelper<T>(IQueryable<T> source, string propertyName, bool descending, bool anotherLevel)
+        {
+            if (string.IsNullOrWhiteSpace(propertyName)) return (IOrderedQueryable<T>)source;
+
+            ParameterExpression param = Expression.Parameter(typeof(T), string.Empty); // I don't care about some naming
+            MemberExpression property = Expression.PropertyOrField(param, propertyName);
+            LambdaExpression sort = Expression.Lambda(property, param);
+            MethodCallExpression call = Expression.Call(
+                typeof(Queryable),
+                (!anotherLevel ? "OrderBy" : "ThenBy") + (descending ? "Descending" : string.Empty),
+                new[] { typeof(T), property.Type },
+                source.Expression,
+                Expression.Quote(sort));
+            return (IOrderedQueryable<T>)source.Provider.CreateQuery<T>(call);
+        }
+
+        public static IOrderedQueryable<T> OrderBy<T>(this IQueryable<T> source, string propertyName)
+        {
+            return OrderingHelper(source, propertyName, false, false);
+        }
+
+        public static IOrderedQueryable<T> OrderByDescending<T>(this IQueryable<T> source, string propertyName)
+        {
+            return OrderingHelper(source, propertyName, true, false);
+        }
+
+        public static IOrderedQueryable<T> ThenBy<T>(this IOrderedQueryable<T> source, string propertyName)
+        {
+            return OrderingHelper(source, propertyName, false, true);
+        }
+
+        public static IOrderedQueryable<T> ThenByDescending<T>(this IOrderedQueryable<T> source, string propertyName)
+        {
+            return OrderingHelper(source, propertyName, true, true);
+        }
+
+        class ParameterRebinder : ExpressionVisitor
+        {
+            readonly Dictionary<ParameterExpression, ParameterExpression> map;
+
+            ParameterRebinder(Dictionary<ParameterExpression, ParameterExpression> map)
+            {
+                this.map = map ?? new Dictionary<ParameterExpression, ParameterExpression>();
+            }
+
+            public static Expression ReplaceParameters(Dictionary<ParameterExpression, ParameterExpression> map, Expression exp)
+            {
+                return new ParameterRebinder(map).Visit(exp);
+            }
+
+            protected override Expression VisitParameter(ParameterExpression p)
+            {
+                ParameterExpression replacement;
+
+                if (map.TryGetValue(p, out replacement))
+                {
+                    p = replacement;
+                }
+
+                return base.VisitParameter(p);
+            }
+        }
+    }
+}
---
+++ NetCoreCommon/Helpers/Utilities.cs
@@ -0,0 +1,93 @@
+﻿using NetCoreCommon.Pagination;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace NetCoreCommon.Helpers
+{
+    /// <summary>
+    /// Utility class
+    /// </summary>
+    public class Utilities
+    {
+        /// <summary>
+        /// Build lambda function to filter records
+        /// </summary>
+        /// <typeparam name="TEntity">Entity type</typeparam>
+        /// <param name="parameters">Parameter to filter</param>
+        /// <returns></returns>
+        public static Expression<Func<TEntity, bool>> Filter<TEntity>(LazyPageParameters parameters)
+        {
+            MethodInfo containsMethod = typeof(string).GetMethods().Where(x => x.Name == "Contains").FirstOrDefault();
+            MethodInfo endsWithMethod = typeof(string).GetMethods().Where(x => x.Name == "EndsWith").FirstOrDefault();    
+            MethodInfo startsWithMethod = typeof(string).GetMethods().Where(x => x.Name == "StartsWith").FirstOrDefault();
+
+            var predicate = PredicateBuilder.True<TEntity>();
+            if (parameters != null && parameters?.Filters != null)
+            {
+                foreach (var item in parameters.Filters)
+                {
+                    if (!item.Key.Equals("globalFilter"))
+                    {
+                        var param = Expression.Parameter(typeof(TEntity));
+                        var member = Expression.Property(param, item.Key);
+
+                        if (!GetConstant(member, item, out UnaryExpression constant)) continue;
+
+                        switch (item.Value.MatchMode)
+                        {
+                            case "equals":
+                                var condition =
+                                    Expression.Lambda<Func<TEntity, bool>>(
+                                        Expression.Equal(member, constant), param
+                                    );//.Compile(); // for LINQ to SQl/Entities skip Compile() call
+                                predicate = predicate.And(condition);
+                                break;
+                            default:
+                                var call = Expression.Call(member,
+                                    item.Value.MatchMode.Equals("contains") ? containsMethod :
+                                    (item.Value.MatchMode.Equals("startsWith") ? startsWithMethod : endsWithMethod),
+                                    constant);
+                                predicate = predicate.And(Expression.Lambda<Func<TEntity, bool>>(call, param));
+
+                                break;
+                        }
+                    }
+                }
+            }
+            return predicate;
+        }
+
+        /// <summary>
+        /// Get expression from member and filter options
+        /// </summary>
+        /// <param name="member">Member expresion</param>
+        /// <param name="filter">Filter option</param>
+        /// <param name="expression">Parameter by ref, unary expresion</param>
+        /// <returns></returns>
+        private static bool GetConstant(MemberExpression member, KeyValuePair<string, LazyPageFilter> filter, out UnaryExpression expression)
+        {
+            expression = null;
+            try
+            {
+                var propertyType = ((PropertyInfo)member.Member).PropertyType;
+                var converter = TypeDescriptor.GetConverter(propertyType); // 1
+
+                if (!converter.CanConvertFrom(typeof(string))) // 2
+                    throw new NotSupportedException();
+
+                var propertyValue = converter.ConvertFromInvariantString(filter.Value.Value); // 3
+                var constant = Expression.Constant(propertyValue);
+                expression = Expression.Convert(constant, propertyType); // 4
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
